011348c7f76ecc8a2e4a54b19d3080de557f6275
agnxsh <iamagnix@gmail.com>
2022-04-25 20:23:32 +0530

    add routing and integrate with backend please :)))

---
+++ codebase/client/src/views/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+export { default as FormView } from './FormView'
+export { default as IndexView } from './IndexView'
+export { default as SigninBusinessView } from './SigninBusinessView'
+export { default as SigninStudentView } from './SigninStudentView'
+export { default as StyleguideView } from './StyleguideView'
+
+/* eslint-enable */
