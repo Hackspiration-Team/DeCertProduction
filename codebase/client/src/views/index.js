ebfc8634c61e07f1415ba617223b797d33396bc4
bartick <bartickmaiti1998@gmail.com>
2022-04-25 23:13:43 +0530

    done router

--- codebase/client/src/views/index.js
+++ codebase/client/src/views/index.js
@@ -1,9 +1,5 @@
-/* eslint-disable */
-
-export { default as FormView } from './FormView'
-export { default as IndexView } from './IndexView'
-export { default as SigninBusinessView } from './SigninBusinessView'
-export { default as SigninStudentView } from './SigninStudentView'
-export { default as StyleguideView } from './StyleguideView'
-
-/* eslint-enable */
+export { default as FormView } from './FormView';
+export { default as IndexView } from './IndexView';
+export { default as SigninBusinessView } from './SigninBusinessView';
+export { default as SigninStudentView } from './SigninStudentView';
+export { default as StyleguideView } from './StyleguideView';
